9f48367b53f141fb2bf8a85c0b4b8cde4ef802be
Morten Winkler Jørgensen <mwjorgensen@welltec.com>
2025-06-25 07:49:00 +0200

    Documented RingBuffer.

--- src/include/kjut/RingBuffer.h
+++ src/include/kjut/RingBuffer.h
@@ -7,12 +7,99 @@
 namespace Kjut
 {
 
+/**
+ * \brief Specifies the behavior of a RingBuffer when it runs out of available space.
+ *
+ * This enum controls how the RingBuffer handles overflow conditions during push operations.
+ */
 enum class OverflowPolicy
 {
+    /**
+     * When data is added using RingBuffer::push() and no space is available,
+     * the new data is discarded, leaving the RingBuffer unchanged.
+     */
     SkipNewData = 0,
+
+    /**
+     * When data is added using RingBuffer::push() and no space is available,
+     * the oldest data in the RingBuffer is overwritten and lost.
+     */
     OverwriteOldestData
 };
 
+
+
+/**
+ * \brief RingBuffer implements a ring buffer for S elements of type T.
+
+### Examples
+
+```cpp
+#include <kjut/RingBuffer.h?>
+
+int main(int argc, char *argv[])
+{
+    Kjut::RingBuffer<int, 4> rb;
+
+    rb.setOverflowPolicy(Kjut::OverflowPolicy::OverwriteOldestData);
+    rb.push(1);
+    rb.push(2);
+    rb.push(3);
+    rb.push(4);
+    rb.push(5);
+    rb.push(6);
+    rb.push(7);
+    rb.push(8);
+    rb.push(9);
+
+    ASSERT_EQ(rb.size(), 4);
+    ASSERT_EQ(6, rb.peek());
+    ASSERT_TRUE(rb.pop());
+
+    ASSERT_EQ(rb.size(), 3);
+    ASSERT_EQ(7, rb.peek());
+    ASSERT_TRUE(rb.pop());
+
+    ASSERT_EQ(rb.size(), 2);
+    ASSERT_EQ(8, rb.peek());
+    ASSERT_TRUE(rb.pop());
+
+    ASSERT_EQ(rb.size(), 1);
+    ASSERT_EQ(9, rb.peek());
+    ASSERT_TRUE(rb.pop());
+
+    ASSERT_EQ(rb.size(), 0);
+
+    return 0;
+}
+
+```
+
+## Thread Safety
+
+RingBuffer is thread safe iff you use the OverflowPolicy::SkipNewData policy.
+Inder this condition, only the thread calling push() will update tehe tail and only the
+thread calling peek() and pop().
+
+\anchor overflow_section
+## Overflows
+
+In RingBuffer context, an overflow is what happens when more elements are added than the instance can hold. In such a case, the \c overRunCallback is invoked with the current parameters, and what happens depends on the set OverflowPolicy:
+
+OverflowPolicy       | Actions
+-------------------- | -------------
+SkipNewData          | The new data for which there are no available place are ignored and the RingBuffer instance is not modified.
+OverwriteOldestData  | The oldest element in the RingBuffer is overwritten and thus lost. The new data is added and becomes the new end of the RingBuffer.
+
+
+## Requirements for T
+
+T must provide the following
+
+ - A default Constructor: \c T()
+ - A public detructor: \c ~T()
+ - An assignment operator: \c T& \c operator=(const T & )
+*/
 
 template <typename T, size_t S>
 class RingBuffer
@@ -21,57 +108,87 @@ class RingBuffer
 
 public:
 
-
-
+    /** \brief Creates an empty RingBuffer.
+    An empty ringbuffer has a fixed capacity and cannot grow nor shrink and supports adding data and taking data.
+     */
     RingBuffer();
+
+    /** \brief Cleans up a RingBuffer.
+    All content from the RingBuffer instance is attempted deleted. The order in which the elements are attempted deleted is not guaranteed and no exceptions are caught or handled in the destructor.
+     */
     virtual ~RingBuffer();
 
-    /// \throws Any exception thrown in ~T(). ~T() may be called if overflowPolicy is OverwriteOldestData.
-    /// Even if ~T() throws, maintenance of the Ringbuffer is guaranteed and it has hed and tail consistent.
+    /**  \brief Pushes a T onto the end of the ringbuffer calling the assignment operator on any existing element being replaced.
+    Even if \c T& \c T::operator=(const T &other) throws, maintenance of the Ringbuffer is guaranteed and it has hed and tail consistent.
+    \throws Anything Any exception thrown in \c T& \c operator=(const T &other) is caught and retrown after proper bookkeeping.
+    */
     bool push(const T &element);
+
+    /** \brief Returns a reference to the firstmost element in the buffer.
+    This method is const and does not change the RingBuffer.
+    \note Even if the RingBuffer is empty, a reference to a garbage element is returned. The content of this element is undefined. A entry in the
+    \param element A const reference to the element of type \c T to push.
+    \returns A reference to the first element in the buffer or a reference to a garbage element.
+    */
     const T &peek() const;
 
-    /// \throws Any exception thrown in ~T()..
-    /// Even if ~T() throws, maintenance of the Ringbuffer is guaranteed and it has hed and tail consistent.
+    /** \brief Deletes the first elemenet in the buffer and decrements its size promoting next element to the head.
+
+    Even if ~T() throws, maintenance of the Ringbuffer is guaranteed and it has hed and tail consistent.
+    \returns True if an element was popped and false if the RungBuffer is empty an nothing could be popped.
+    \throws Any exception thrown in ~T()..
+    */
     bool pop() ;
 
+    /** \brief Returns the number of T instances currently in the RingBuffer.
+    \return The number of T instances currently in the RingBuffer.
+    */
     size_t size() const;
-    size_t sizeAvailable() const;
-    size_t capacity() const;
-    void setOverRunCallBack(void (*overRunCallback)(const RingBuffer &));
 
+    /** \brief Returns the number of T instances this Ringbuffer can currently have pushed before running out of space.
+    \return Returns the number of T instances this Ringbuffer can currently have pushed before running out of space.
+    */
+    size_t sizeAvailable() const;
+
+
+    /** \brief Returns maximum the number of elements this RingBuffer instance can hold. This is effectively \c S.
+     \returns The maximum number of elements this RingBuffer instance can hold.
+    */
+    size_t capacity() const;
+
+    /** \brief Sets the function pointer to call in case of an \link overflow_section overflow \endlink.
+
+    \param overRunCallback The function to call in case of overflow.
+    \param buffer A reference to the RingBuffer instance in which the overflow occurred.
+    \param currentHeadIndex Current index of the head when overflow occurred.
+    \param currentTailIndex Current index of the tail when overflow occurred.
+    \param element A reference to the element that caused the overflow.
+*/
+    void setOverRunCallBack(void (*overRunCallback)(const RingBuffer & buffer, size_t currentHeadIndex, size_t currentTailIndex, const T& element));
+
+    /** \brief Returns the current OverflowPolicy.
+    \returns The current OverflowPolicy of this RingBuffer instance.
+    \see The section on \link overflow_section overflow \endlink.*/
     OverflowPolicy overflowPolicy() const { return d.overflowPolicy;}
+
+    /** \brief Sets the OverflowPolicy.
+    \param The new OverfloPolicy.
+    \see The section on \link overflow_section overflow \endlink. */
     void  setOverflowPolicy(OverflowPolicy newPolicy) { d.overflowPolicy = newPolicy; }
 
 //private:
+///@cond
 
-
-    mutable struct
+    KJUT_VOLATILE struct
     {
-
-        void incrementHeadIndexWithPotentialWraparound()
-        {
-            headIndex++;
-            if(headIndex >= S)
-            {
-                headIndex = 0;
-            }
-        }
-        void incrementTailIndexWithPotentialWraparound()
-        {
-            tailIndex++;
-            if(tailIndex >= S)
-            {
-                tailIndex = 0;
-            }
-        }
-        void (*overflowCallback)(const RingBuffer &) = nullptr;
+        void (*overflowCallback)(const RingBuffer &, size_t currentHeadIndex, size_t currentTailIndex, const T& element) = nullptr;
         Array<T, S+1> data;
         size_t headIndex = 0;
         size_t tailIndex = 0;
         const size_t Capacity = S + 1;
         OverflowPolicy overflowPolicy = OverflowPolicy::OverwriteOldestData;
     } d;
+///@endcond
 };
 
 
@@ -91,13 +208,13 @@ template <typename T, size_t S>
 bool RingBuffer<T,S>::push(const T &element)
 {
     size_t next = (d.tailIndex + 1) % d.Capacity;
-
+    size_t nextHeadIndex = d.headIndex;
     if (next == d.headIndex) {
         if (d.overflowPolicy == OverflowPolicy::SkipNewData)
         {
             if(d.overflowCallback)
             {
-                d.overflowCallback(*this);
+                d.overflowCallback(*this, d.headIndex, d.tailIndex, element);
             }
             return false;
         }
@@ -105,26 +222,24 @@ bool RingBuffer<T,S>::push(const T &element)
         {
             if(d.overflowCallback)
             {
-                d.overflowCallback(*this);
+                d.overflowCallback(*this, d.headIndex, d.tailIndex, element);
             }
-            d.headIndex = (d.headIndex + 1) % d.Capacity;
+            nextHeadIndex = (d.headIndex + 1) % d.Capacity;
         }
     }
 
-#define RINGBUFFER_PUSH_BOOK_KEEPING \
-    d.tailIndex = (d.tailIndex + 1) % d.Capacity;
 
     try
     {
         d.data[d.tailIndex] = element;
-        RINGBUFFER_PUSH_BOOK_KEEPING;
+        d.tailIndex = (d.tailIndex + 1) % d.Capacity;
+        d.headIndex = nextHeadIndex;
     }
     catch(...)
     {
-        RINGBUFFER_PUSH_BOOK_KEEPING;
+        //Do not update change the buffer. The element is now in undefined state.
         throw;
     }
-#undef RINGBUFFER_PUSH_BOOK_KEEPING
 
 
     return true;
@@ -136,7 +251,7 @@ template <typename T, size_t S>
 const T &RingBuffer<T,S>::peek() const
 {
     if (d.headIndex == d.tailIndex) {
-        throw std::runtime_error("Buffer is empty");    //MWJ: Log and return default/.
+        return d.data[S+1]; //This is the 'default' element. Garbage.
     }
     return d.data[d.headIndex];
 }
@@ -144,12 +259,23 @@ const T &RingBuffer<T,S>::peek() const
 template <typename T, size_t S>
 bool RingBuffer<T,S>::pop()
 {
-    if (d.headIndex == d.tailIndex) {
+    if (d.headIndex == d.tailIndex)
+    {
         return false; // buffer is empty
     }
-    d.headIndex = (d.headIndex+ 1) % d.Capacity;    //MWJ: Destroy
+    try
+    {
+        d.data[d.headIndex] = T();
+        d.headIndex = (d.headIndex+ 1) % d.Capacity;
+    }
+    catch (...)
+    {
+        d.headIndex = (d.headIndex+ 1) % d.Capacity;
+        throw;
+    }
     return true;
 }
+
 
 template <typename T, size_t S>
 size_t RingBuffer<T,S>::size() const
@@ -171,7 +297,7 @@ size_t RingBuffer<T,S>::sizeAvailable() const
 
 
 template <typename T, size_t S>
-void RingBuffer<T,S>::setOverRunCallBack(void (*overRunCallback)(const RingBuffer &))
+void RingBuffer<T,S>::setOverRunCallBack(void (*overRunCallback)(const RingBuffer &, size_t currentHeadIndex, size_t currentTailIndex, const T& element))
 {
     d.overflowCallback = overRunCallback;
 }
@@ -188,7 +314,6 @@ void RingBuffer<T,S>::setOverRunCallBack(void (*overRunCallback)(const RingBuffe
 
 template <typename T, size_t S>
 std::ostream &operator<<(std::ostream &os, const Kjut::RingBuffer<T, S> &b) {
-
     os << "[";
     for(size_t i = 0; i < S; i++)
     {
--- tests/tst_ringbuffer.cpp
+++ tests/tst_ringbuffer.cpp
@@ -101,3 +101,194 @@ TEST(suiteName, test_pushes_and_pops_with_skip_new_data_policy)
     ASSERT_EQ(rb.size(), 0);
 }
 
+
+
+
+std::vector<std::vector<int>> invocations;
+
+void test_callbacks_with_skip_new_data_policy_callback(const Kjut::RingBuffer<int, 4> &, size_t currentHeadIndex, size_t currentTailIndex, const int& element)
+{
+    invocations.push_back({(int(currentHeadIndex)), int(currentTailIndex), element});
+}
+
+
+TEST(suiteName, test_callbacks_with_skip_new_data_policy)
+{
+    Kjut::RingBuffer<int, 4> rb;
+    invocations.clear();
+    rb.setOverRunCallBack(test_callbacks_with_skip_new_data_policy_callback);
+    rb.setOverflowPolicy(Kjut::OverflowPolicy::SkipNewData);
+    rb.push(1);
+    rb.push(2);
+    rb.push(3);
+    rb.push(4);
+    rb.push(5);
+    rb.push(6);
+
+    std::vector<std::vector<int>> expected = {
+        {0,4,5},
+        {0,4,6}
+    };
+
+    ASSERT_EQ(invocations, expected);
+
+}
+
+
+TEST(suiteName, test_callbacks_with_overwrite_old_data_policy)
+{
+    Kjut::RingBuffer<int, 4> rb;
+    invocations.clear();
+    rb.setOverRunCallBack(test_callbacks_with_skip_new_data_policy_callback);
+    rb.setOverflowPolicy(Kjut::OverflowPolicy::OverwriteOldestData);
+    rb.push(1);
+    rb.push(2);
+    rb.push(3);
+    rb.push(4);
+    rb.push(5);
+    rb.push(6);
+
+    std::vector<std::vector<int>> expected = {
+        {0,4,5},
+        {1,0,6}
+    };
+
+    ASSERT_EQ(invocations, expected);
+}
+
+
+#define POP_AND_ASSERT_EQ(expected) ASSERT_EQ(int(rb.peek()), expected); ASSERT_TRUE(rb.pop());
+
+TEST(suiteName, test_lots_of_pushes_and_pops_with_overwrite_policy)
+{
+    Kjut::RingBuffer<int, 4> rb;
+    rb.setOverflowPolicy(Kjut::OverflowPolicy::OverwriteOldestData);
+
+    rb.push(1);
+    rb.push(2);
+    rb.push(3);
+    rb.push(4);
+
+    POP_AND_ASSERT_EQ(1);
+    POP_AND_ASSERT_EQ(2);
+    POP_AND_ASSERT_EQ(3);
+    POP_AND_ASSERT_EQ(4);
+
+    rb.push(5);
+    rb.push(6);
+    rb.push(7);
+    rb.push(8);
+    rb.push(9);
+    rb.push(10);
+
+    POP_AND_ASSERT_EQ(7);
+    POP_AND_ASSERT_EQ(8);
+    POP_AND_ASSERT_EQ(9);
+    POP_AND_ASSERT_EQ(10);
+
+
+
+
+
+
+
+}
+
+
+class ThrowingAssignmentOperator
+{
+public:
+    ThrowingAssignmentOperator()
+    {
+    }
+
+    ThrowingAssignmentOperator(int value, bool shouldThrow)
+    {
+        this->value = value;
+        shallThrowWhenAssignedTo = shouldThrow;
+    }
+
+    ThrowingAssignmentOperator(int value)
+    {
+        this->value = value;
+    }
+
+    ThrowingAssignmentOperator & operator=(const ThrowingAssignmentOperator& other)
+    {
+        const int oldValue = ThrowingAssignmentOperator::value;
+        this->value = THIS_IS_UNDEFINED;
+        if(shallThrowWhenAssignedTo)
+        {
+            throw oldValue;
+        }
+        this->value = other.value;
+        this->shallThrowWhenAssignedTo = other.shallThrowWhenAssignedTo;
+        return *this;
+    }
+
+    explicit operator int() const
+    {
+        return value;
+    }
+    mutable bool shallThrowWhenAssignedTo = false;
+    int value = -1;
+
+    static constexpr int THIS_IS_UNDEFINED = -31415;
+
+};
+
+std::ostream &operator<<(std::ostream &os, const ThrowingAssignmentOperator &e) {
+    os << e.value;
+    os << (e.shallThrowWhenAssignedTo ? "T" : "F");
+    return os;
+}
+
+
+
+TEST(suiteName, test_set_integrity_maintained_when_T_destructor_throws_exceptions_during_push)
+{
+    Kjut::RingBuffer<ThrowingAssignmentOperator, 4> rb ;
+    rb.setOverflowPolicy(Kjut::OverflowPolicy::OverwriteOldestData);
+    rb.push( 10 );
+    rb.push( 20 );
+    rb.push( 30);
+    rb.push( 40 );
+
+    rb.peek().shallThrowWhenAssignedTo = true;
+
+    rb.push( 50 ); //this does not overwrite, as we have S+1 capacity.
+    ASSERT_THROW( rb.push( 60 ), int ); //This should cause 10 to be assigned to causing an exception.
+
+    POP_AND_ASSERT_EQ(20);
+    POP_AND_ASSERT_EQ(30);
+    POP_AND_ASSERT_EQ(40);
+    POP_AND_ASSERT_EQ(50);
+    ASSERT_EQ(rb.size(), 0);
+
+}
+
+
+TEST(suiteName, test_set_integrity_maintained_when_T_assignment_operator_throws_exceptions_during_pop)
+{
+    Kjut::RingBuffer<ThrowingAssignmentOperator, 4> rb;
+    rb.setOverflowPolicy(Kjut::OverflowPolicy::OverwriteOldestData);
+    rb.push( 10 );
+    rb.push( 20 );
+    rb.push( 30 );
+    rb.push( 40 );
+
+    rb.peek().shallThrowWhenAssignedTo = true;
+
+    int caught = 0;
+    try
+    {
+        rb.pop(); //This should cause 10 to be deleted.
+    }
+    catch(int ten)
+    {
+       caught = ten;
+    }
+    ASSERT_EQ(caught, 10);
+}
+
+
