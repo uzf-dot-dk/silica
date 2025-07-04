660e64346bfef4e54c9117cfc2bda32c1a594e52
Morten W. Jørgensen <mwjorgensen@welltec.com>
2025-07-04 12:05:56 +0200

    Add documentation to Mutex.

--- src/include/kjut/Mutex.h
+++ src/include/kjut/Mutex.h
@@ -24,6 +24,8 @@ namespace Kjut
 {
 
 
+/** \brief Mutex provices a synchronization mechanism that can prevent simultaneously access to shared data.
+ */
 class Mutex
 {
 
@@ -31,11 +33,20 @@ class Mutex
     DISABLE_MOVE(Mutex);
 
 public:
+    /** \brief Creates a new unlocked Mutex. */
     Mutex();
     ~Mutex();
 
+    /** \brief Locks this Mutex.
+     *
+     *  Calling lock() will prevent further execution in the calling thread until this Mutex can be locked. */
     void lock();
+
+    /** \brief Unlocks this Mutex allowing it to be locked again. */
     void unlock();
+
+    /** \brief Tries to lock this Mutex but does not block if this Mutex is already locked.
+     *  \returns True if this Mutex could be locked, false if not. */
     bool tryLock();
 
 private:
