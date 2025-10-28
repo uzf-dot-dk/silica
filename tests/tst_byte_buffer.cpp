8a09454e6d08b40b3fe7840fe77bb4772422f527
Morten Winkler Jørgensen <mwjorgensen@welltec.com>
2025-10-28 08:35:21 +0100

    Add tests of ByteBuffer with static arrays.

--- tests/tst_byte_buffer.cpp
+++ tests/tst_byte_buffer.cpp
@@ -46,9 +46,6 @@ TEST(suiteName, test_basic_writing_one_byte_at_a_time_with_dynamic_array_as_sour
         ASSERT_EQ(byteCount, 1);
         ASSERT_EQ(emitter, iod);
     }
-
-
-
 }
 
 TEST(suiteName, test_basic_writing_an_array_with_dynamic_array_as_source)
@@ -75,4 +72,48 @@ TEST(suiteName, test_basic_writing_an_array_with_dynamic_array_as_source)
 }
 
 
+
+
+TEST(suiteName, test_basic_reading_with_static_array_as_source)
+{
+    Kjut::Array<Kjut::Byte, 6> readSource = {'W', 'a', 's', 's', 'u', 'p'};
+    Kjut::ByteBuffer theBuffer(&readSource);
+    Kjut::IODevice* iod = &theBuffer;
+
+
+    Kjut::Array<Kjut::Byte, 6> actuallyRead;
+    while(iod->canReadMore())
+    {
+        actuallyRead.append(iod->read());
+    }
+
+    ASSERT_EQ(actuallyRead,  readSource);
+}
+
+TEST(suiteName, test_basic_writing_one_byte_at_a_time_with_static_array_as_source)
+{
+    Kjut::Array<Kjut::Byte, 20> notUsed;
+    Kjut::Array<Kjut::Byte, 20> destination;
+
+    Kjut::ByteBuffer theBuffer(&notUsed, &destination);
+    Kjut::IODevice* iod = &theBuffer;
+    SignalSpy<Kjut::IODevice*, size_t> writeWatcher;
+    writeWatcher.spyOn(&(iod->writeComplete));
+
+    Kjut::Array<Kjut::Byte, 20> expected = {'S', 'o', 'm', 'e', ' ', 'd', 'a', 't','a'};
+    for(unsigned char c: expected)
+    {
+        iod->writeByte(c);
+    }
+
+    ASSERT_EQ(destination,  expected);
+    ASSERT_EQ(writeWatcher.invocationCount(), expected.size());
+    for(auto invocation : writeWatcher.invocations())
+    {
+        size_t byteCount = std::get<size_t>(invocation);
+        Kjut::IODevice * emitter = std::get<Kjut::IODevice *>(invocation);
+        ASSERT_EQ(byteCount, 1);
+        ASSERT_EQ(emitter, iod);
+    }
+}
 
