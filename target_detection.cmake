dfd168ac64448d60375059819e08c627b9215c45
Morten Winkler <morten@winkler.dk>
2026-02-08 12:54:07 +0100

    Add detection of baremetal for RPxyzw chips.

--- target_detection.cmake
+++ target_detection.cmake
@@ -1,13 +1,24 @@
-if(WIN32)
+set(SILICA_TARGET_ARCH "unknown")
+
+if(CMAKE_SYSTEM_NAME STREQUAL "PICO")
+    set(SILICA_TARGET_OS "baremetal")
+    if(CMAKE_CXX_COMPILER MATCHES "arm.*none.*eabi")
+        set(SILICA_TARGET_ARCH "arm")
+    endif()
+
+elseif(WIN32)
     set(SILICA_TARGET_OS "windows")
-elseif(UNIX AND NOT APPLE)
-    set(SILICA_TARGET_OS "linux")
 elseif(APPLE)
     set(SILICA_TARGET_OS "macos")
+elseif(UNIX)
+    set(SILICA_TARGET_OS "linux")
 else()
     set(SILICA_TARGET_OS "none")
 endif()
 
+
+
+message( INFO " Silica is ${SILICA_TARGET_OS} on ${SILICA_TARGET_ARCH}" )
 
 # x86_64	64-bit x86	AMD64
 # AMD64	64-bit x86 (MSVC)	AMD64
